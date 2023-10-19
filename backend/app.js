9698a51ded24758c1cd5db9aca6469f3ddd7f944
mattg101112 <147451520+mattg101112@users.noreply.github.com>
2023-10-18 21:32:33 -0500

    call to api is working
    call to api is working need to purchase tokens

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
+const OpenAIApi = require('openai');
 const express = require('express');
 const app = express();
 
 app.get('/api/data', (req, res) => {
-  console.log("in api/data")
+  /*console.log("in api/data")
   const data = { message: 'Hello from the server!' };
   res.json(data);
+  */
+    async function main() {
+        const openai = new OpenAIApi();
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "system", content: "You are a helpful assistant." }],
+            model: "gpt-3.5-turbo",
+        });
+
+        console.log(completion.choices[0]);
+    }
+
+    main();
+
 });
 
 app.listen(3000, () => {
