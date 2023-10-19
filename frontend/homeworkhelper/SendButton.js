0d664ae5cf579566c9c4fdd76d94315f5e117988
mattg101112 <147451520+mattg101112@users.noreply.github.com>
2023-10-19 17:34:13 -0500

    text input working
    Text input working

--- frontend/homeworkhelper/SendButton.js
+++ frontend/homeworkhelper/SendButton.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, TextInput, StyleSheet } from 'react-native';
 
 const SendButton = () => {
   const [data, setData] = useState(null);
+  const [userInput, setUserInput] = useState(null);
 
   const dataToSend = {
     message: "What is a healthy meal that I can eat?",
@@ -18,7 +19,7 @@ const SendButton = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          message: "What is a healthy meal that I can eat?",
+          message: userInput,
         }),
       });
       console.error("Hi dataToSend" + dataToSend);
@@ -33,12 +34,28 @@ const SendButton = () => {
 
   return (
     <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your message"
+        value={userInput}
+        onChangeText={(text) => setUserInput(text)} // Capture user input
+      />
       <Button title="Fetch Data" onPress={fetchData} />
       {console.error("data: " + data)}
-      {data && <Text>{JSON.stringify(data)}</Text>}
+      {data && <Text>{JSON.stringify(data.content)}</Text>}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    margin: 10,
+  }
+
+})
 
 export default SendButton;
 
