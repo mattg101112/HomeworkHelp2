cb4483dcb5eb8aa4356db86614cb4a05c4abc668
mattg101112 <147451520+mattg101112@users.noreply.github.com>
2023-10-19 00:12:08 -0500

    api working but returned data not displaying
    api working but returned data not displaying

--- frontend/homeworkhelper/App.js
+++ frontend/homeworkhelper/App.js
@@ -4,11 +4,22 @@ import SendButton from './SendButton';
 
 const App = () => {
   return (
-    <View>
-      <Text>My App</Text>
+    <View style={Styles.container}>
+      <Text>Submit your homework question and Chatgpt will help you</Text>
       <SendButton />
     </View>
   );
 };
 
+
+const Styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+  },
+};
+
 export default App;
+
